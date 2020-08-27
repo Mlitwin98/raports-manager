776e84bd7853dff3a074daebec97cedf0a5e7bd8
Mateusz Litwin <m_litwin@wp.pl>
2020-08-27 12:43:44 +0200

    Added edit option for raports

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path('login', views.login, name='login'),
 	path('logout', views.logout, name='logout'),
 	path('raports_maker', views.raports_maker, name='raports_maker'),
-	path('raports_view', views.raports_view, name='raports_view')
+	path('raports_view', views.raports_view, name='raports_view'),
+	path('raports_edit/<int:raport_id>', views.raports_edit, name='raports_edit')
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
 from datetime import datetime
 from .models import Raport
 
@@ -22,7 +23,7 @@ def login(request):
 			auth.login(request, user)
 			return redirect('index')
 		else:
-			messages.info(request, 'invalid credentials')
+			messages.info(request, 'Błędne dane logowania')
 			return redirect('login')
 	else:
 		return render(request, 'login.html')
@@ -34,53 +35,84 @@ def logout(request):
 
 
 def raports_maker(request):
-	if request.method == "POST":
-		title = request.POST['title']
-		content = request.POST['content']
-		author_name = request.user.first_name
-		author_lastname = request.user.last_name
-		if title != '' and content != '' and not title.isspace() and not content.isspace():
-			raport = Raport.create(title, content, author_name, author_lastname)
-			raport.save()
-			messages.info(request, 'Raport wysłany')
-			return redirect('raports_maker')
+	if request.user.is_authenticated:
+		if request.user.is_superuser:
+			if request.method == "POST":
+				title = request.POST['title']
+				content = request.POST['content']
+				author_name = request.user.first_name
+				author_lastname = request.user.last_name
+				if title != '' and content != '' and not title.isspace() and not content.isspace():
+					raport = Raport.create(title, content, author_name, author_lastname)
+					raport.save()
+					messages.info(request, 'Raport wysłany')
+					return redirect('raports_maker')
+				else:
+					messages.info(request, 'Tytuł i treść nie mogą być puste')
+					return redirect('raports_maker')
+			else:
+				return render(request, 'raports_maker.html')
 		else:
-			messages.info(request, 'Tytuł i treść nie mogą być puste')
-			return redirect('reports_maker')
+			return redirect('index')
 	else:
-		return render(request, 'raports_maker.html')
+		return redirect('login')
 
 
 def raports_view(request):
-	if request.method == "POST":
-		search_title = request.POST['title']
-		search_content = request.POST['content']
-		search_name = request.POST['name']
-		search_lastname = request.POST['lastname']
-		search_date = request.POST['date']
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			search_title = request.POST['title']
+			search_content = request.POST['content']
+			search_name = request.POST['name']
+			search_lastname = request.POST['lastname']
+			search_date = request.POST['date']
 
-		raports = Raport.objects.all()
+			raports = Raport.objects.all()
 
-		if search_title != '' and not search_title.isspace():
-			raports = Raport.objects.filter(title__icontains=search_title)
-		if search_content != '' and not search_content.isspace():
-			raports = raports.filter(content__icontains=search_content)
-		if search_name != '' and not search_name.isspace():
-			raports = raports.filter(author_name__iexact=search_name)
-		if search_lastname != '' and not search_lastname.isspace():
-			raports = raports.filter(author_lastname__iexact=search_lastname)
-		if search_date != '':
-			dates = search_date.split(' - ')
-			dateStart = datetime.strptime(dates[0], '%m/%d/%Y').date()
-			dateEnd = datetime.strptime(dates[1], '%m/%d/%Y').date()
+			if search_title != '' and not search_title.isspace():
+				raports = Raport.objects.filter(title__icontains=search_title)
+			if search_content != '' and not search_content.isspace():
+				raports = raports.filter(content__icontains=search_content)
+			if search_name != '' and not search_name.isspace():
+				raports = raports.filter(author_name__iexact=search_name)
+			if search_lastname != '' and not search_lastname.isspace():
+				raports = raports.filter(author_lastname__iexact=search_lastname)
+			if search_date != '':
+				dates = search_date.split(' - ')
+				dateStart = datetime.strptime(dates[0], '%m/%d/%Y').date()
+				dateEnd = datetime.strptime(dates[1], '%m/%d/%Y').date()
 
-			raports = raports.filter(dateTime__gte=dateStart)
-			raports = raports.filter(dateTime__lte=dateEnd)
-		return render(request, 'raports_view.html', {"raports": raports})
+				raports = raports.filter(dateTime__gte=dateStart)
+				raports = raports.filter(dateTime__lte=dateEnd)
+			return render(request, 'raports_view.html', {"raports": raports})
+		else:
+			visibleRaports = 50
+			numOfRaports = Raport.objects.all().count()
+			if numOfRaports < visibleRaports:
+				visibleRaports = numOfRaports
+			raports = Raport.objects.all()[numOfRaports-visibleRaports:numOfRaports]
+			return render(request, 'raports_view.html', {"raports": raports})
 	else:
-		visibleRaports = 50
-		numOfRaports = Raport.objects.all().count()
-		if numOfRaports < visibleRaports:
-			visibleRaports = numOfRaports
-		raports = Raport.objects.all()[numOfRaports-visibleRaports:numOfRaports]
-		return render(request, 'raports_view.html', {"raports": raports})
+		return redirect('login')
+
+
+def raports_edit(request, raport_id):
+	if request.user.is_authenticated:
+		raport = get_object_or_404(Raport, id=raport_id)
+		if request.method == "POST":
+			title = request.POST['title']
+			content = request.POST['content']
+			if title != '' and content != '' and not title.isspace() and not content.isspace():
+				raport.title = title
+				raport.content = content
+				raport.author_name = request.user.first_name
+				raport.author_lastname = request.user.last_name
+				raport.save()
+				return redirect('raports_view')
+			else:
+				messages.info(request, 'Tytuł i treść nie mogą być puste')
+				return render(request, 'raports_edit.html', {"raport": raport})
+		else:
+			return render(request, 'raports_edit.html', {"raport": raport})
+	else:
+		return redirect('login')
