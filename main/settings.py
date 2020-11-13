f2b603a7923f6326e0e689aa98c353c7c10a7fe0
Mateusz Litwin <m_litwin@wp.pl>
2020-11-13 13:09:17 +0100

    Update settings

--- main/settings.py
+++ main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'moz3%7opte&uvo@o_i5xxr_qt9l#=13okn%s&t3vfln($l5&&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '10.0.0.175']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main/static')
 ]
